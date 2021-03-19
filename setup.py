31bbec5d55437d36b78ca1c36dee19e74203695b
Peter Li <phli@google.com>
2021-03-18 18:26:29 -0700

    Add ffn.training.models and switch to Pillow.
    It is still easier to use this repo via git clone rather than pip
    install, e.g. from Colab. But this seemed worth updating anyway.

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setup(
 	version		= '0.1.0',
 	author		= 'Michal Januszewski',
 	author_email	= 'mjanusz@google.com',
-	packages	= ['ffn', 'ffn.inference', 'ffn.training', 'ffn.utils'],
+	packages	= ['ffn', 'ffn.inference', 'ffn.training', 'ffn.training.models', 'ffn.utils'],
 	scripts		= ['build_coordinates.py', 'compute_partitions.py', 'run_inference.py', 'train.py'],
 	url		= 'https://github.com/google/ffn',
 	license		= 'LICENSE',
 	description	= 'Flood-Filling Networks for volumetric instance segmentation',
 	long_description= open('README.md').read(),
-	install_requires= ['scikit-image', 'scipy', 'numpy', 'tensorflow', 'h5py', 'PIL', 'absl-py'],
+	install_requires= ['scikit-image', 'scipy', 'numpy', 'tensorflow<2', 'h5py', 'Pillow', 'absl-py'],
 )
